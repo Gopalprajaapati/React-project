7e3ea99edbf17ab6a6ac9b885bb5a260baf2b35c
gopal25119 <gopalprajapati25119@gmail.com>
2025-11-28 23:47:41 +0530

    Componenats cerate and mutiple time use but isuue not changes and solution are use parent to data childe format flow exmple app ---> card --->imag processing but not img to card

---
+++ understading props/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from './Components/Card'
+
+const App = () => {
+  return (
+    <div>
+      <div className="parent">
+      
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      
+    
+    </div>
+    </div>
+  )
+}
+
+export default App
---
+++ understading props/src/Components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+     <div>
+          <div className="card">
+        <img src="https://cms.imgworlds.com/assets/9f39cd74-e665-45e5-9c59-b482d5223a01.jpg" alt="" />
+        <h1>Gopal prajapti</h1>
+        <p>Lorem ipsum dolor 
+          sit amet consectetur adipisicing elit. Quos ullam dolor explicabo eius at repudiandae ipsa quia temporibus!</p>
+          <button>click</button>
+      </div>  </div>
+    
+   
+  )
+}
+
+export default Card
